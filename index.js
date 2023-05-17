692c201cc980f04f0c1eb123dcbafb596bf17b8d
Datagate <datagate_service@example.com>
2023-05-17 10:52:14 +0800

    debug

--- index.js
+++ index.js
@@ -14,25 +14,37 @@ async function runCommands() {
     const fs = require("fs");
     // 对 git_source_key 进行 Base64 解码
     const decodedKey_source = Buffer.from(git_source_key, "base64").toString();
-    const decodedKey_remote = Buffer.from(git_remote_key, "base64").toString();
-    console.log(decodedKey_source);
+    console.log("git_source_key " + git_source_key);
+    console.log("source key after decode " + decodedKey_source);
     // 将解码后的密钥存储到 /tmp/git_source_key 文件中
-    await fs.writeFile("/tmp/git_source_key", decodedKey_source, { mode: 0o400 }, (err) => {
-      if (err) {
-        console.error("无法将密钥存储到文件：", err);
-        return;
+    await fs.writeFile(
+      "/tmp/git_source_key",
+      decodedKey_source,
+      { mode: 0o400 },
+      (err) => {
+        if (err) {
+          console.error("无法将密钥存储到文件：", err);
+          return;
+        }
+        console.log("密钥已成功存储到 /tmp/git_source_key 文件。");
       }
-      console.log("密钥已成功存储到 /tmp/git_source_key 文件。");
-    });
+    );
 
-    console.log(decodedKey_remote);
-    await fs.writeFile("/tmp/git_remote_key", decodedKey_remote, { mode: 0o400 }, (err) => {
-      if (err) {
-        console.error("无法将密钥存储到文件：", err);
-        return;
+    const decodedKey_remote = Buffer.from(git_remote_key, "base64").toString();
+    console.log("git_remote_key " + git_remote_key);
+    console.log("remote key after decode " + decodedKey_remote);
+    await fs.writeFile(
+      "/tmp/git_remote_key",
+      decodedKey_remote,
+      { mode: 0o400 },
+      (err) => {
+        if (err) {
+          console.error("无法将密钥存储到文件：", err);
+          return;
+        }
+        console.log("密钥已成功存储到 /tmp/git_remote_key 文件。");
       }
-      console.log("密钥已成功存储到 /tmp/git_remote_key 文件。");
-    });
+    );
 
     console.log("Running: rm -rf ./code");
     await exec("rm -rf ./code");
